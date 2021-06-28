05eae1a33675e577b73b12f55a3c77021e5ce752
Gabriel Barban Rocha <gabrielbarban@MacBook-Air-de-Gabriel.local>
2021-06-28 03:01:34 -0300

    Buscando um cliente, e criando uma cobranca no Asaas

--- app/Http/Controllers/MostraDadosController.php
+++ app/Http/Controllers/MostraDadosController.php
@@ -11,6 +11,67 @@ class MostraDadosController extends Controller
 
     public function exibemsg($id)
     {
-        return "Ola mundo, eu sou o Lumen 5.8.<br>Voce digitou o numero: {$id}";
+        // carrego os dados
+        $apiKey = "APIKEY";
+        $nome = "FICATRANQUILO SERVICOS DE CONTABILIDADE LTDA";
+        $cpfCnpj = "10245240000100";
+        $email = "contato@ficatranquilo.com.br";
+
+        //pego o cliente
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.asaas.com/api/v3/customers?cpfCnpj=".$cpfCnpj);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+          "access_token: ".$apiKey
+        ));
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $Cliente = json_decode($response);
+        $ClienteID = 0;
+        foreach($Cliente->data as $cliente){
+            echo "Nome do Cliente: ".$cliente->name."<br>";
+            echo "ID do Cliente: ".$cliente->id."<br>";
+            $ClienteID = $cliente->id;
+        }
+        echo "<br><Br><br><Br><br><Br>";
+
+
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.asaas.com/api/v3/payments");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "{
+          \"customer\": \"{$ClienteID}\",
+          \"billingType\": \"BOLETO\",
+          \"dueDate\": \"2021-07-10\",
+          \"value\": 100,
+          \"description\": \"bla bla bla qualquer coisa\",
+          \"externalReference\": \"056984\",
+          \"discount\": {
+            \"value\": 10,
+            \"dueDateLimitDays\": 0
+          },
+          \"fine\": {
+            \"value\": 1
+          },
+          \"interest\": {
+            \"value\": 2
+          },
+          \"postalService\": true
+        }");
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+          "Content-Type: application/json",
+          "access_token: ".$apiKey
+        ));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
     }
 }
